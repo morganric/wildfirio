ad7a5d3e30d0f2fb9ee2dc02efc08e15dec98789
rich <morganric@gmail.com>
2015-03-20 21:34:02 +0000

    this

--- app/js/config.js
+++ app/js/config.js
@@ -11,11 +11,11 @@ angular.module('myApp.config', [])
   .constant('loginRedirectPath', '/login')
 
   // your Firebase data URL goes here, no trailing slash
-  .constant('FBURL', 'https://INSTANCE.firebaseio.com')
+  .constant('FBURL', 'https://dazzling-torch-7635.firebaseio.com')
 
   // double check that the app has been configured before running it and blowing up space and time
   .run(['FBURL', '$timeout', function(FBURL, $timeout) {
-    if( FBURL.match('//INSTANCE.firebaseio.com') ) {
+    if( FBURL.match('https//dazzling-torch-7635.firebaseio.com') ) {
       angular.element(document.body).html('<h1>Please configure app/js/config.js before running!</h1>');
       $timeout(function() {
         angular.element(document.body).removeClass('hide');
